01ae73c89e155539294e6d9178a6d3b00b278359
Hafiz <haasad13@gmail.com>
2026-07-07 07:48:50 +0600

    chore: update android package name to match firebase configuration

--- frontend/android/app/src/main/java/com/studyabroadchat/MainActivity.kt
+++ frontend/android/app/src/main/java/com/studyabroadchat/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.abroadinquiry
+package com.studyabroadchat
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- frontend/android/app/src/main/java/com/studyabroadchat/MainApplication.kt
+++ frontend/android/app/src/main/java/com/studyabroadchat/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.abroadinquiry
+package com.studyabroadchat
 
 import android.app.Application
 import com.facebook.react.PackageList
